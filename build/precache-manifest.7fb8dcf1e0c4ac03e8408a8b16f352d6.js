1f0e7b60ef038a8bde7d0c6e62ccda61e2b8c847
thanigaivel97 <thani@lewayhertz.com>
2020-09-25 13:44:01 +0530

    latest usiing apis

---
+++ build/precache-manifest.7fb8dcf1e0c4ac03e8408a8b16f352d6.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9063ee014ee9c08bb9d5534492c5cc9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a8372a5126cfea583d1",
+    "url": "/static/css/main.2ec3e62c.chunk.css"
+  },
+  {
+    "revision": "47b586c8bd8deec69e7c",
+    "url": "/static/js/2.c289362d.chunk.js"
+  },
+  {
+    "revision": "e96fdedfb1c1c0a665f6f0e54ddb461c",
+    "url": "/static/js/2.c289362d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a8372a5126cfea583d1",
+    "url": "/static/js/main.ece026d3.chunk.js"
+  },
+  {
+    "revision": "cf96df3fa8303c9b2f13",
+    "url": "/static/js/runtime-main.53bdce6a.js"
+  },
+  {
+    "revision": "a055ee9b09fa66679976ea996e280b94",
+    "url": "/static/media/Screenshot 2020-06-30 at 8.57.06 AM.a055ee9b.png"
+  },
+  {
+    "revision": "f25b8a55a38a0c7dd013d0cc1a4e481c",
+    "url": "/static/media/awards.f25b8a55.png"
+  },
+  {
+    "revision": "67417d070865e64e62961841bb250ea6",
+    "url": "/static/media/awardstitlle.67417d07.png"
+  },
+  {
+    "revision": "a7565003601eedd38280a4cca2622287",
+    "url": "/static/media/insta.a7565003.png"
+  },
+  {
+    "revision": "d507f2d5dad49f0589a862512b78ee8d",
+    "url": "/static/media/logo.d507f2d5.png"
+  }
+]);
